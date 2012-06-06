babcb7604c8e1ce62437395ed05fbd4fd28330c2
Carsten Peter Meyer <meyerca@uni-mainz.de>
2012-06-06 19:03:21 +0000

    macro to produce histos out of lists containing CLs values

---
+++ macros/contourplot/makecontourhists.C
@@ -0,0 +1,20 @@
+#include "contourmacros/m0_vs_m12_nofloat.C"
+
+void makecontourhists(const TString& combo = "all" /*"0lepton"*/ /*"1lepton"*/) 
+{
+  //const char* ehistfile = m0_vs_m12_nofloat("MySimpleChannelAnalysisOutput_hypotest__1_harvest_list");
+  //const char* ehistfile = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_hypotest__1_harvest_list");
+//   const char* ehistfile2 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_ee_global_allsys_hypotest__1_harvest_list");
+//   const char* ehistfile3 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_emu_global_allsys_hypotest__1_harvest_list");
+//   const char* ehistfile4 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_mumu_global_allsys_hypotest__1_harvest_list");
+//   const char* ehistfile5 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_hypotest__1_harvest_list");
+//   const char* ehistfile5 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_SR4_hypotest__1_harvest_list");
+//   const char* ehistfile5 = m0_vs_m12_nofloat("Merged_GMSB_combined__1_harvest_list");
+
+//   const char* ehistfile5 = m0_vs_m12_nofloat("Merged_Output_hypotest__1_harvest_list");
+  const char* ehistfile5 = m0_vs_m12_nofloat("../mergedList_Down");
+
+
+   //  const char* ehistfile6 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_toys_hypotest__1_harvest_list");
+}
+
