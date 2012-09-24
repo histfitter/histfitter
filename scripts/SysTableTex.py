9021ff79a46bde93a0cc3b0c33949be5e96e46ac
Max Baak <maxbaak@gmail.com>
2012-09-24 21:15:22 +0000

    Alex' table scripts for tutorial 25-9-2012

---
+++ scripts/SysTableTex.py
@@ -0,0 +1,83 @@
+
+def tablefragment(m,table,signalRegions,skiplist,chanStr,showPercent):
+  tableline = ''
+
+  tableline += '''
+\\begin{table}
+\\begin{center}
+\\setlength{\\tabcolsep}{0.0pc}
+\\begin{tabular*}{\\textwidth}{@{\\extracolsep{\\fill}}l'''
+
+  for region in signalRegions:
+    tableline += "c"   
+  tableline += '''}
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+{\\bf %s channel}                                   ''' % (table)
+
+  for region in signalRegions:
+    tableline += " & " + region + "           "   
+
+  tableline += ''' \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%'''
+
+  tableline += '''
+Total statistical $(\\sqrt{N_{\\rm obs}})$             '''
+  for region in signalRegions:
+    tableline += " & $\\pm " + str(("%.2f" %m[region]['sqrtnobsa'])) + "$       "
+  tableline += '''\\\\
+%%'''
+
+  tableline += '''
+Total background systematic              '''
+
+  for region in signalRegions:
+    tableline += " & $\\pm " + str(("%.2f" %m[region]['totsyserr'])) + "$       "
+
+  tableline += '''      \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%''' 
+
+
+  doAsym=False
+  m_listofkeys = m[signalRegions[0]].keys()
+  m_listofkeys.sort()
+  for name in m_listofkeys:
+    if name not in skiplist:
+      printname = name
+      printname = printname.replace('syserr_','')
+      printname = printname.replace('_','\_')
+      for index,region in enumerate(signalRegions):
+        if index == 0:
+          tableline += "\n" + printname + "      "
+          
+        if not showPercent:
+          tableline += "   & $\\pm " + str(("%.2f" %m[region][name])) + "$       "
+        else:
+          percentage = m[region][name]/m[region]['totsyserr'] * 100.0
+          if percentage <1:
+            tableline += "   & $\\pm " + str(("%.2f" %m[region][name])) + " [" + str(("%.2f" %percentage)) + "\\%] $       "
+          else:
+            tableline += "   & $\\pm " + str(("%.2f" %m[region][name])) + " [" + str(("%.1f" %percentage)) + "\\%] $       "
+                    
+          
+        if index == len(signalRegions)-1:
+          tableline += '''\\\\
+%%'''
+
+
+  tableline += '''
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+\\end{tabular*}
+\\end{center}
+\\caption[Breakdown of uncertainty on background estimates]{
+Breakdown of the dominant systematic uncertainties on background estimates in the various signal regions.
+Note that the individual uncertainties can be correlated, and do not necessarily add up quadratically to 
+the total background uncertainty.
+\\label{table.results.bkgestimate.uncertainties.%s}}
+\\end{table}
+%%''' % (chanStr) 
+    
+  return tableline
+
---
+++ scripts/YieldsTableTex.py
@@ -0,0 +1,252 @@
+import sys
+
+def exampletable():
+
+  m = {
+  'names' :		( 'SR',		'TR',		'WR',		'TR + WR',	'SR (LnT)' ),
+  'nobs' : 		( 1, 		80, 		202, 		282, 		1464 ),
+  'Fitted_bkg_events' : ( 1.81, 0.75, 	80, 9, 		202, 14, 	282, 17, 	1464, 38 ),
+
+  'Fitted_top_events' : ( 1.34, 0.52, 	65.0, 12.3, 	31.8, 15.8, 	96.7, 25.9, 	40.1, 11.3 ), 
+  'Fitted_WZ_events' : ( 0.47, 0.40, 	11.2, 4.6, 	160.9, 27.4, 	172.1, 31.2, 	169.7, 34.1 ),
+  'Fitted_top_WZ_events' : ( 1.81, 0.69, 	76.2, 11.8, 	192.6, 24.3, 	268.8, 32.1, 	209.8, 33.8 ),
+  'Fitted_QCD_events' : ( 0.0, 0.3, 0.0, 	3.7, 7.6, 	9.4, 19.6, 	13.0, 27.2, 	1254.2, 51.3 ),
+  'MC_exp_SM_events' : ( 1.75, 		77.76,  	189.15, 	266.91, 	1848.38 ),
+  'MC_exp_top_events' : ( 1.29, 		62.9,		31.0,		93.93,		38.94 ),
+  'MC_exp_WZ_events' : ( 0.46,		10.2,		146,		156.29,		154.10 ), 
+  'MC_exp_QCD_events' : ( 0.00, 		4.67,		12.02,		16.69,		1655.34 ),
+  }
+
+  return m
+
+
+
+def tablefragment(m, channel, signalregionslist,sampleList,showBeforeFitError):
+
+  tableline = ''
+  
+  tableline += '''
+\\begin{tabular*}{\\textwidth}{@{\\extracolsep{\\fill}}l'''
+
+  for region in m['names']:
+    tableline += "r"   
+  
+  tableline += '''}
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+{\\bf %s channel}          ''' %channel
+  for region in m['names']:
+    tableline += " & " + region + "           "   
+
+  tableline += '''   \\\\[-0.05cm]
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%''' 
+
+  tableline += '''
+Observed events         '''
+  for n in m['nobs']:
+    tableline += " & $" + str(int(n)) + "$             "
+
+  tableline +='''       \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%'''
+
+  
+#########  #########
+########## fitted
+######### #########
+  
+  tableline += '''
+Fitted bkg events        '''
+  for index, n in enumerate(m['TOTAL_FITTED_bkg_events']):
+    if m['names'][index] in signalregionslist:
+      tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['TOTAL_FITTED_bkg_events_err'][index])) +  "$         "
+    else:
+      tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['TOTAL_FITTED_bkg_events_err'][index])) +  "$         "
+  tableline +='''     \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%'''
+
+
+  map_listofkeys = m.keys()
+#  map_listofkeys.sort()
+
+
+  for sample in sampleList:
+    for name in map_listofkeys:
+      if "Fitted_events_" in name and sample in name:
+        sample = name.replace("Fitted_events_","")
+        tableline += '''
+        Fitted '''
+        tableline += sample
+        tableline += ''' events        '''
+        for index, n in enumerate(m[name]):
+          if m['names'][index] in signalregionslist:
+            tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['Fitted_err_'+sample][index])) +  "$         "
+            #          tableline +='''     \\\\
+            # %%'''
+          else:
+            tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['Fitted_err_'+sample][index])) +  "$         "
+        tableline +='''     \\\\
+%%'''
+
+
+  
+#########  #########
+########## expected
+######### #########
+
+  tableline +='''     
+ \\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%'''
+
+
+  tableline += '''
+MC exp. SM events             '''
+  for index, n in enumerate(m['TOTAL_MC_EXP_BKG_events']):
+    if showBeforeFitError:
+      tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['TOTAL_MC_EXP_BKG_err'][index])) +  "$         "
+    else:
+      tableline += " & $" + str(("%.2f" %n)) +  "$         "
+  tableline +='''     \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+%%''' 
+
+
+  map_listofkeys = m.keys()
+#  map_listofkeys.sort()
+
+
+  for sample in sampleList:
+    for name in map_listofkeys:
+      if "MC_exp_events_" in name and sample in name:
+        sample = name.replace("MC_exp_events_","")
+        tableline += '''
+        MC exp. '''
+        tableline += sample
+        tableline += ''' events        '''
+        for index, n in enumerate(m[name]):
+          if m['names'][index] in signalregionslist:
+            if showBeforeFitError:
+              tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['MC_exp_err_'+sample][index])) +  "$         "
+            else:
+              tableline += " & $" + str(("%.2f" %n)) +  "$         "
+          else:
+            if showBeforeFitError:
+              tableline += " & $" + str(("%.2f" %n)) + " \\pm " + str(("%.2f" %m['MC_exp_err_'+sample][index])) +  "$         "
+            else:
+              tableline += " & $" + str(("%.2f" %n)) +  "$         "
+        tableline +='''     \\\\
+%%'''
+
+
+  tableline +='''     \\\\
+\\noalign{\\smallskip}\\hline\\noalign{\\smallskip}
+\\end{tabular*}
+%%''' 
+
+  return tableline
+
+
+################
+
+
+def tablestart():
+
+  start = '''
+
+\\begin{table}
+\\begin{center}
+\\setlength{\\tabcolsep}{0.0pc}
+{\\small
+%%'''
+
+  return start
+
+
+def tableend(signalregion='3+ jets, loose',suffix='sr3jl'):
+
+  end = '''%%
+}
+\\end{center}
+\\caption{Signal region: %s. Fit results for the electron (top part) and muon (bottom part) channels, for an integrated luminosity of $1035$\,\ipb.
+The results are obtained from the control regions using the discovery fit (see text for details). The fit results of the loose-not-tight regions are not shown.
+Nominal MC expectations (normalised to MC cross-sections) are given for comparison. 
+The Monte Carlo QCD estimates are provided for illustrational purposes only, and are not used in the fit.
+The errors shown are the statistical plus systematic uncertainties, except for the error on the background estimate in the signal region, which is the systematic uncertainty only.}
+\\label{table.results.systematics.in.logL.fit.%s}
+\\end{table}
+%%''' % (signalregion,suffix)
+
+  return end
+
+
+
+def tableend2(signalregion='3+ jets, loose',suffix='sr3jl'):
+
+  end = '''%%
+}
+\\end{center}
+\\caption{Signal region: %s. Fit results for an integrated luminosity of $1035$\,\ipb.
+The results are obtained from the control regions using the discovery fit (see text for details). 
+Nominal MC expectations (normalised to MC cross-sections) are given for comparison. 
+The Monte Carlo QCD estimates are provided for illustrational purposes only, and are not used in the fit.
+The errors shown are the statistical plus systematic uncertainties, except for the error on the background estimate in the signal region, which is the systematic uncertainty only.
+All presented errors have been derived using MINOS.}
+\\label{table.results.systematics.in.logL.fit.%s}
+\\end{table}
+%%''' % (signalregion,suffix)
+
+  return end
+
+
+
+
+def tableend3(suffix='sr3jl'):
+
+  end = '''%%
+}
+\\end{center}
+\caption{ Background fit results for the S3 (top part) and S4 (bottom part) signal regions, for an integrated luminosity of $4.7$~\\ifb.
+%%The results are obtained from the control regions using the discovery fit (see text for details). The fit results of the loose-not-tight regions are not shown.
+Nominal MC expectations (normalised to MC cross-sections) are given for comparison. 
+%%The Monte Carlo QCD estimates are provided for illustrational purposes only, and are not used in the fit.
+The errors shown are the statistical plus systematic uncertainties for control regions, while only the statistical errors are shown for signal and validation regions, in the case of a background only fit.}
+%%, except for the error on the background estimate in the signal region, which is the systematic uncertainty only.}
+\\label{table.results.systematics.in.logL.fit.%s}
+\\end{table}
+%%''' %(suffix)
+
+  return end
+
+
+def tableend4(regionsList, suffix='sr3jl'):
+
+  end = '''%%
+}
+\\end{center}
+\caption{ Background fit results for the '''
+
+  nRegions = len(regionsList)
+  for index, region in enumerate(regionsList):
+    if index == 0 :
+      end +=  region
+    elif index < nRegions-1:
+      end += ", " + region 
+    else:
+      end += " and " + region
+      
+  end += ''' regions, for an integrated luminosity of $4.7$~\\ifb.
+%%The results are obtained from the control regions using the discovery fit (see text for details). The fit results of the loose-not-tight regions are not shown.
+Nominal MC expectations (normalised to MC cross-sections) are given for comparison. 
+%%The Monte Carlo QCD estimates are provided for illustrational purposes only, and are not used in the fit.
+The errors shown are the statistical plus systematic uncertainties.}
+%%, except for the error on the background estimate in the signal region, which is the systematic uncertainty only.}
+\\label{table.results.yields.fit.%s}
+\\end{table}
+%%''' %(suffix)
+
+  return end
+
+#print tablestart(), tableframent(m), tableend()
+
+
