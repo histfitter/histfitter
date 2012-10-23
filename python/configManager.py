c6e58cd6f95ee74d5358610ec1a719786ffd6900
Geert-Jan Besjes <gbesjes@gmail.com>
2012-10-23 13:21:09 +0000

    small bug with undef var in configManager.py

--- python/configManager.py
+++ python/configManager.py
@@ -679,7 +679,7 @@ class ConfigManager(object):
                     if syst.isMerged():
                         chan.getSample(sam.name).addShapeSys(syst.name,nomMergedName,highMergedName,lowMergedName,syst.constraint)
                         syst.Reset()
-                    chan.getSample(sam.name).shapeSystList.append((systName, nomMergedName+"Norm", syst.constraint, "", "", "", ""))
+                    chan.getSample(sam.name).shapeSystList.append((syst.name, nomMergedName+"Norm", syst.constraint, "", "", "", ""))
             else:
                 chan.getSample(sam.name).addShapeSys(syst.name, nomName, highName, lowName)
                 chan.getSample(sam.name).shapeSystList.append((syst.name, nomName+"Norm", syst.constraint, "", "", "", ""))
