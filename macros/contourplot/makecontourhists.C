c78d03c9a6784e784a0823541cdf548fddc535d0
Zach Marshall <zachary.louis.marshall@gmail.com>
2012-11-02 13:33:18 +0000

    Cleanup and notes on general use

--- macros/contourplot/makecontourhists.C
+++ macros/contourplot/makecontourhists.C
@@ -1,20 +1,18 @@
 #include "contourmacros/m0_vs_m12_nofloat.C"
 
-void makecontourhists(const TString& combo = "all" /*"0lepton"*/ /*"1lepton"*/) 
+/********************************
+
+REMEMBER that this file uses summary_harvest_tree_description.h 
+to determine how to read your list files.  You will need to regenerate
+that file in order to read a list file with a different format from
+makelistfiles.C
+
+********************************/
+
+void makecontourhists(const TString& combo = "all" ) 
 {
-  //const char* ehistfile = m0_vs_m12_nofloat("MySimpleChannelAnalysisOutput_hypotest__1_harvest_list");
-  //const char* ehistfile = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_hypotest__1_harvest_list");
-//   const char* ehistfile2 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_ee_global_allsys_hypotest__1_harvest_list");
-//   const char* ehistfile3 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_emu_global_allsys_hypotest__1_harvest_list");
-//   const char* ehistfile4 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_mumu_global_allsys_hypotest__1_harvest_list");
-//   const char* ehistfile5 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_hypotest__1_harvest_list");
-//   const char* ehistfile5 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_SR4_hypotest__1_harvest_list");
-//   const char* ehistfile5 = m0_vs_m12_nofloat("Merged_GMSB_combined__1_harvest_list");
-
-//   const char* ehistfile5 = m0_vs_m12_nofloat("Merged_Output_hypotest__1_harvest_list");
-  const char* ehistfile5 = m0_vs_m12_nofloat("../mergedList_Down");
-
-
-   //  const char* ehistfile6 = m0_vs_m12_nofloat("2lepSusyFitterAnalysisOutput_combined_global_allsys_toys_hypotest__1_harvest_list");
+  const char* ehistfile;
+  ehistfile = m0_vs_m12_nofloat("Merged_Output_1_hypotest__1_harvest_list");
+  ehistfile = m0_vs_m12_nofloat("Merged_Output_2_hypotest__1_harvest_list");
 }
 
