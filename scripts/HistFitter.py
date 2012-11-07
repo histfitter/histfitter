8f168d2e7227886c640e6a30f6c903333f5a1e89
Geert-Jan Besjes <gbesjes@gmail.com>
2012-11-07 15:08:53 +0000

    arguments now through argparse

--- scripts/HistFitter.py
+++ scripts/HistFitter.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
 from ROOT import gROOT,gSystem,gDirectory,RooAbsData,RooRandom,RooWorkspace
 gSystem.Load("libSusyFitter.so")
 from ROOT import ConfigMgr
 gROOT.Reset()
-from logger import Logger
+import sys
 
 from logger import Logger
 log = Logger('HistFitter')
-
 
 def enum(typename, field_names):
     "Create a new enumeration type"
@@ -161,7 +163,6 @@ def GenerateFitAndPlot(fc, drawBeforeAfterFit):
 if __name__ == "__main__":
     from configManager import configMgr
     from prepareHistos import TreePrepare, HistoPrepare
-    log = Logger("HistFitter")
 
     configMgr.readFromTree = False
     configMgr.executeHistFactory = False
@@ -183,135 +184,135 @@ if __name__ == "__main__":
     
     print "\n * * * Welcome to HistFitter * * *\n"
 
-    import os, sys
-    import getopt
-    def usage():
-        print "HistFitter.py [-L loglevel] [-i] [-t] [-w] [-x] [-f] [-l] [-p] [-d] [-n nTOYs] [-s seed] [-r SRs] [-g gridPoint] [-b bkgParName,value] [--draw plotOpts] <configuration_file>\n"
-        print "(all OFF by default. Turn steps ON with options)"
-        print "-L   set log level (VERBOSE, DEBUG, INFO, WARNING, ERROR, FATAL, ALWAYS; default INFO)"
-        print "-t   re-create histograms from TTrees (default: %s)" % (configMgr.readFromTree)
-        print "-w   re-create workspace from histograms (default: %s)" % (configMgr.executeHistFactory)
-        print "-x   use XML and hist2workspace, instead of HistFactory python methods, to generate workspaces (default: %s)" % (configMgr.writeXML)
-        print "-f   fit the workspace (default: %s)" % (configMgr.executeHistFactory)
-        print "-n   <nTOYs> sets number of TOYs (<=0 means to use real data, default: %i)" % configMgr.nTOYs
-        print "-s   <number> set the random seed for toy generation (default is CPU clock: %i)" % configMgr.toySeed
-        print "-a   use Asimov dataset for fitting and plotting (default: %i)" % configMgr.useAsimovSet
-        print "-i   stays in interactive session after executing the script (default %s)" % runInterpreter
-        print "-l   make limit plot of workspace (default %s)" % printLimits
-        print "-p   run (exclusion) hypothesis test on workspace (default %s)" % doHypoTests
-        print "--pz run the discovery hypothesis test (default %s)" % (not doUL)
-        print "-g   <grid points to be processed> - give as comma separated list"
-        print "-r   signal region to be processed - give as comma separated list (default = all)"
-        print "-d   Draw before/after fit plots of all channels (default: %s)" % drawBeforeAfterFit,  "  additional arguments 'allPlots' or comma separated 'beforeAfter, corrMatrix, sepComponents, likelihood'"
-        print "-b   when doing hypotest, correct bkg-level to: bkg strength parameter, bkg value"
-        print "-0   removes empty bins when drawing the data histograms with (complimentary to -d)"
-        print "--ty run toys (default with mu)."
-        print "--draw with arguments 'allPlots' or comma separated 'beforeAfter, corrMatrix, sepComponents, likelihood'"
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("configFile", help="configuration file to execute")
+    parser.add_argument("-L", "--log-level", help="set log level", choices=["VERBOSE", "DEBUG", "INFO", "WARNING", "ERROR", "FATAL", "ALWAYS"])
+    parser.add_argument("-F", "--fit-type", help="type of fit to run", choices=["bkg", "disc", "excl"])
+    parser.add_argument("-t", "--create-histograms", help="re-create histograms from TTrees", action="store_true", default=configMgr.readFromTree)
+    parser.add_argument("-w", "--create-workspace", help="re-create workspace from histograms", action="store_true", default=configMgr.executeHistFactory)
+    parser.add_argument("-x", "--use-XML", help="write XML files by hand and call hist2workspace on them, instead of directly writing workspaces", action="store_true", default=configMgr.writeXML)
+    parser.add_argument("-f", "--fit", help="fit the workspace", action="store_true", default=configMgr.executeHistFactory)
+    parser.add_argument("-n", "--num_toys", type=int, help="set the number of toys, <=0 means to use real data", default=configMgr.nTOYs)
+    parser.add_argument("-s", "--seed", type=int, help="set the random seed for toy generation", default=configMgr.toySeed)
+    parser.add_argument("-a", "--use-asimov", help="use Asimov dataset for fitting and plotting", action="store_true", default=configMgr.useAsimovSet)
+    parser.add_argument("-i", "--interactive", help="remain in interactive mode after running", action="store_true", default=runInterpreter)
+    parser.add_argument("-l", "--limit-plot", help="make limit plot of workspace", action="store_true", default=printLimits)
+    parser.add_argument("-p", "--hypotest", help="run exclusion hypothesis test", action="store_true", default=doHypoTests)
+    parser.add_argument("-z", "--discovery-hypotest", help="run discovery hypothesis test", action="store_true", default=not doUL)
+    parser.add_argument("-g", "--grid_points", help="grid points to process (comma-seperated)")
+    parser.add_argument("-r", "--regions", help="signal regions to process (comma-seperated)", default="all")
+    parser.add_argument("-d", "--draw", choices=["allPlots", "beforeAfter", "corrMatrix", "sepComponents", "likelihood"], help="plots to draw")
+    parser.add_argument("-b", "--background", help="when doing hypotest, set background levels to values, form of bkgParName,value")
+    parser.add_argument("-0", "--no-empty", help="do not draw empty bins when drawing", action="store_true")
+    parser.add_argument("-T", "--run-toys", help="run toys (default with mu)", action="store_true")
+    parser.add_argument("-V", "--validation", help="include validation regions", action="store_true")
 
-        print "\nAlso see the README file.\n"
-        print "Command examples:"
-        print "HistFitter.py -i python/MySusyFitterConfig.py           #only runs initialization in interactive mode (try e.g.: configMgr.<tab>)"
-        print "HistFitter.py -t -w -f python/MySusyFitterConfig.py     #runs all steps (TTree->Histos->Workspace->Fit) in batch mode"
-        print "HistFitter.py -f -i python/MySusyFitterConfig.py        #only fit and plot, using existing workspace, in interactive session"
-        print "HistFitter.py -s 666 -f python/MySusyFitterConfig.py    #fit a TOY dataset (from seed=666) and prints RooFitResult"
-        print "\nNote: examples of input TTrees can be found in /afs/cern.ch/atlas/groups/susy/1lepton/samples/"
-        sys.exit(0)
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "xd0twfinals:r:b:g:L:p:",["bkgfit","exclfit","discfit","vr","pz","ty","draw="])
-        configFile = str(args[0])
-    except:
-        usage()
-
+    args = parser.parse_args()
     
-    for opt,arg in opts:
-        #print opt,arg
-        if opt == '--bkgfit':
-            myFitType=FitType.Background
-        elif opt == '--exclfit':
-            myFitType=FitType.Exclusion
-        elif opt == '--discfit':
-            myFitType=FitType.Discovery
-        elif opt == '--vr':
-            doValidation=True
-        elif opt == '-t':
-            configMgr.readFromTree=True
-        elif opt == '-w':
-            configMgr.executeHistFactory = True
-        elif opt == '-x':
-            configMgr.writeXML = True
-        elif opt == '-f':
-            runFit = True
-        elif opt == '-n':
-            configMgr.nTOYs = int(arg)
-        elif opt == '-i':
-            runInterpreter = True
-        elif opt == '-L':
-            #2nd argument forces the log level to be locked for python scripts
-            if len(arg) == 1: #assume this is an integer log level
-                log.setLevel(int(arg),True)
-            else:
-                log.setLevel(arg,True) #can take both strings and ints, does its own checking 
-        elif opt == '-l':
-            printLimits = True
-        elif opt == '-p':
-            doHypoTests = True
-        elif opt == '--pz':
-            doHypoTests = True
-            doUL = False
-        elif opt == '-d':
+    if args.fit_type == "bkg":
+        myFitType = FitType.Background
+    elif args.fit_type == "excl":
+        myFitType = FitType.Exclusion
+    elif args.fit_type == "disc":
+        myFitType = FitType.Discovery
+
+    if args.validation:
+        doValidation = True
+
+    if args.create_histograms:
+        configMgr.readFromTree = True
+
+    if args.create_workspace:
+        configMgr.executeHistFactory = True
+
+    if args.use_XML:
+        configMgr.writeXML = True
+
+    if args.fit:
+        runFit = True
+
+    configMgr.nTOYs = args.num_toys
+
+    if args.interactive:
+        runInterpreter = True
+
+    if args.log_level:
+        log.setLevel(args.log_level, True) #do not add a default to args.log_level or we will always lock it
+
+    if args.limit_plot:
+        printLimits = True
+
+    if args.hypotest:
+        doHypoTests = True
+
+    if args.discovery_hypotest:
+        doHypoTests = True
+        doUL = False
+
+    if args.draw:
+        drawBeforeAfterFit = True
+        drawArgs = args.draw.split(',')
+        #print "drawArgs(",drawArgs, ")  length = ", len(drawArgs)
+        if len(drawArgs) == 1 and drawArgs[0] == "allPlots":
             drawBeforeAfterFit = True
-        elif opt == '--draw':
-            drawArgs = arg.split(',')
-            #print "drawArgs(",drawArgs, ")  length = ", len(drawArgs)
-            if len(drawArgs) == 1 and drawArgs[0] == "allPlots":
-                drawBeforeAfterFit = True
-                drawCorrelationMatrix         = True
-                drawSeparateComponents = True
-                drawLogLikelihood               = True
-            elif len(drawArgs)>0:
-                for drawArg in drawArgs:
-                    if drawArg == "beforeAfter":
-                        drawBeforeAfterFit = True
-                    elif drawArg == "corrMatrix":
-                        drawCorrelationMatrix         = True
-                    elif drawArg == "sepComponents":
-                        drawSeparateComponents = True
-                    elif drawArg == "likelihood":
-                        drawLogLikelihood               = True
-                    else:
-                        log.fatal("Wrong draw argument: %s\n  Possible draw arguments are 'allPlots' or comma separated 'beforeAfter, corrMatrix, sepComponents, likelihood'" % drawArg)
-        elif opt == '-0':
-            configMgr.removeEmptyBins = True
-        elif opt == '-s':
-            configMgr.toySeedSet = True
-            configMgr.toySeed = int(arg)
-        elif opt == '-a':
-            configMgr.useAsimovSet = True
-        elif opt == '-g':
-            sigSamples = arg.split(',')
-        elif opt == '-r':
-            pickedSRs = arg.split(',')
-        elif opt == '--ty':
-            runToys = True
-        elif opt == '-b':
-            bkgArgs = arg.split(',')
-            if len(bkgArgs) == 2:
-                configMgr.SetBkgParName(bkgArgs[0])
-                configMgr.SetBkgCorrVal(float(bkgArgs[1]))
-                configMgr.SetBkgChlName("")
-            elif len(bkgArgs) >= 3 and len(bkgArgs) % 3 == 0:
-                for iChan in xrange(len(bkgArgs) / 3):
-                    iCx = iChan * 3
-                    configMgr.AddBkgChlName(bkgArgs[iCx])
-                    configMgr.AddBkgParName(bkgArgs[iCx+1])
-                    configMgr.AddBkgCorrVal(float(bkgArgs[iCx+2]))
-                    continue
-        pass
+            drawCorrelationMatrix = True
+            drawSeparateComponents = True
+            drawLogLikelihood = True
+        elif len(drawArgs)>0:
+            for drawArg in drawArgs:
+                if drawArg == "beforeAfter":
+                    drawBeforeAfterFit = True
+                elif drawArg == "corrMatrix":
+                    drawCorrelationMatrix = True
+                elif drawArg == "sepComponents":
+                    drawSeparateComponents = True
+                elif drawArg == "likelihood":
+                    drawLogLikelihood = True
+                else:
+                    log.fatal("Wrong draw argument: %s\n  Possible draw arguments are 'allPlots' or comma separated 'beforeAfter, corrMatrix, sepComponents, likelihood'" % drawArg) #should now be caught by argparse --GJ 7/11/2012
+
+    if args.no_empty:
+        configMgr.removeEmptyBins = True
+
+    if args.seed != 0: #0 is default because type is int
+        configMgr.toySeedSet = True
+        configMgr.toySeed = args.seed
+
+    if args.use_asimov:
+        configMgr.useAsimovSet = True
+
+    if args.grid_points and args.grid_points != "":
+        sigSamples = args.grid_points.split(",")
+
+    if args.regions and args.regions != "" and args.regions != "all":
+        pickedSRs = args.regions.split(",")
+    else:
+        pickedSRs = None #TODO do we want this?
+
+    print pickedSRs
+
+    if args.run_toys:
+        runToys = True
+
+    if args.background:
+        bkgArgs = args.background.split(',')
+        if len(bkgArgs) == 2:
+            configMgr.SetBkgParName(bkgArgs[0])
+            configMgr.SetBkgCorrVal(float(bkgArgs[1]))
+            configMgr.SetBkgChlName("")
+        elif len(bkgArgs) >= 3 and len(bkgArgs) % 3 == 0:
+            for iChan in xrange(len(bkgArgs) / 3):
+                iCx = iChan * 3
+                configMgr.AddBkgChlName(bkgArgs[iCx])
+                configMgr.AddBkgParName(bkgArgs[iCx+1])
+                configMgr.AddBkgCorrVal(float(bkgArgs[iCx+2]))
+                continue
+        
     gROOT.SetBatch(not runInterpreter)
 
     #mandatory user-defined configuration
-    execfile(configFile)
+    execfile(args.configFile)
 
     #standard execution from now on.
     configMgr.initialize()
