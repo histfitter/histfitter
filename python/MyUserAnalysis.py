3ca96361bfeaa98d93f9b910c5bac2e4dde80890
Max Baak <maxbaak@gmail.com>
2013-02-28 09:33:07 +0000

    sherpa w weight

--- python/MyUserAnalysis.py
+++ python/MyUserAnalysis.py
@@ -18,15 +18,15 @@ ROOT.SetAtlasStyle()
 ##########################
 
 # Set observed and expected number of events in counting experiment
-ndata     =  7. 	# Number of events observed in data
-nbkg      =  5.	 	# Number of predicted bkg events
-nsig      =  5.  	# Number of predicted signal events
-nbkgErr   =  1.  	# (Absolute) Statistical error on bkg estimate
-nsigErr   =  2.  	# (Absolute) Statistical error on signal estimate
+ndata     =  124. 	# Number of events observed in data
+nbkg      =  104.	 	# Number of predicted bkg events
+nsig      =  1.  	# Number of predicted signal events
+nbkgErr   =  12.  	# (Absolute) Statistical error on bkg estimate
+nsigErr   =  1.  	# (Absolute) Statistical error on signal estimate
 lumiError = 0.039 	# Relative luminosity uncertainty
 
 # Set uncorrelated systematics for bkg and signal (1 +- relative uncertainties)
-ucb = Systematic("ucb", configMgr.weights, 1.2,0.8, "user","userOverallSys")
+ucb = Systematic("ucb", configMgr.weights, 1.115,0.885, "user","userOverallSys")
 ucs = Systematic("ucs", configMgr.weights, 1.1,0.9, "user","userOverallSys")
 
 # correlated systematic between background and signal (1 +- relative uncertainties)
@@ -55,20 +55,20 @@ configMgr.weights = "1."
 
 # Define samples
 bkgSample = Sample("Bkg",kGreen-9)
-bkgSample.setStatConfig(True)
+bkgSample.setStatConfig(False)
 bkgSample.buildHisto([nbkg],"UserRegion","cuts")
-bkgSample.buildStatErrors([nbkgErr],"UserRegion","cuts")
-bkgSample.addSystematic(corb)
+#bkgSample.buildStatErrors([nbkgErr],"UserRegion","cuts")
+#bkgSample.addSystematic(corb)
 bkgSample.addSystematic(ucb)
 
 sigSample = Sample("Sig",kPink)
-sigSample.setNormFactor("mu_Sig",1.,0.,10.)
-sigSample.setStatConfig(True)
-sigSample.setNormByTheory()
+sigSample.setNormFactor("mu_Sig",1.,0.,100.)
+sigSample.setStatConfig(False)
+sigSample.setNormByTheory(False)
 sigSample.buildHisto([nsig],"UserRegion","cuts")
-sigSample.buildStatErrors([nsigErr],"UserRegion","cuts")
-sigSample.addSystematic(cors)
-sigSample.addSystematic(ucs)
+#sigSample.buildStatErrors([nsigErr],"UserRegion","cuts")
+#sigSample.addSystematic(cors)
+#sigSample.addSystematic(ucs)
 
 dataSample = Sample("Data",kBlack)
 dataSample.setData()
@@ -82,6 +82,7 @@ ana.setSignalSample(sigSample)
 # Define measurement
 meas = ana.addMeasurement(name="NormalMeasurement",lumi=1.0,lumiErr=lumiError)
 meas.addPOI("mu_Sig")
+meas.addParamSetting("Lumi",True,1)
 
 # Add the channel
 chan = ana.addChannel("cuts",["UserRegion"],1,0.,1.)
