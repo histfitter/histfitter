a155350ed2ff85afce0c80fd445411688f6d0edb
Max Baak <maxbaak@gmail.com>
2013-10-28 13:26:06 +0000

    Update slc6. Fix to function that resets nominal global measurements for mc stat errors.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 # setup ROOT
 # check Root environment setup. Allow for external setup script.
 
-export BUILD="x86_64-slc5-gcc46-opt"
+export BUILD="x86_64-slc6-gcc46-opt"
 export ROOTVERSION="5.34.09"
 export CERNPREFIX="/afs/cern.ch/"
-export PYTHONVERSION="2.6.5" # "2.7.3"
+export PYTHONVERSION="2.7.3"
+export GCCVERSION="4.6"
 
 # Must have gcc and python setup outside of ROOTSYS def for batch running!
 # This section here is cern specific.
-if [[ `hostname` = l*.cern.ch ]]; then 
+
+if [[ `hostname -f` = b*.cern.ch ]] || [[ `hostname -f` = l*.cern.ch ]]; then 
     #don't check for *.cern.ch - any machine at CERN, incl. your laptop, has that hostname!
     echo "INFO: hostname matches l*.cern.ch: setting up gcc and python from afs"
-    # first, setup gcc to version 4.3
-    echo "Setting up gcc version 4.6 ..."
-    source $CERNPREFIX/sw/lcg/external/gcc/4.6/x86_64-slc5/setup.sh
+    # first, setup gcc 
+    echo "Setting up gcc version ${GCCVERSION} ..."
+    source $CERNPREFIX/sw/lcg/external/gcc/${GCCVERSION}/x86_64-slc6/setup.sh
     # second, setup an uptodate python version
     echo "Setting up python version ${PYTHONVERSION} ..."
     export PATH="$CERNPREFIX/sw/lcg/external/Python/${PYTHONVERSION}/$BUILD/bin:${PATH}"
@@ -26,17 +28,17 @@ if [ ! $ROOTSYS ]; then
   echo "With build ${BUILD} ..."
   export CWD=$PWD
   # setup corresponding root
-  cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root-$ROOTVERSION/
-  #cd $CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/
+  #cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root-$ROOTVERSION/
+  cd $CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/
   source bin/thisroot.sh
   cd $CWD
   # setup xrootd on top of this
   export PATH=$CERNPREFIX/sw/lcg/external/xrootd/3.1.0p2/$BUILD/bin:$PATH
-  export LD_LIBRARY_PATH=$CERNPREFIX/sw/lcg/external/xrootd/3.1.0p2/$BUILD/lib64:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH="$CERNPREFIX/sw/lcg/external/xrootd/3.1.0p2/$BUILD/lib64:$LD_LIBRARY_PATH"
   # hack for xrootd: libNetx library from pre-installed root version
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/lib
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/lib"
   # missing libraries on lxbatch machines
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/extlibs64
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/extlibs64"
 fi
 
 # check Root environment setup 
