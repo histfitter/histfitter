a99b2bc27c612cb856ab3bc8a78140467f0430c0
Geert-Jan Besjes <gbesjes@gmail.com>
2013-12-05 13:42:46 +0000

    new log

--- python/configManager.py
+++ python/configManager.py
@@ -14,10 +14,12 @@ log = Logger('ConfigManager')
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+    except:
+        pass
+    #except OSError as exc: # Python >2.5
+        #if exc.errno == errno.EEXIST and os.path.isdir(path):
+            #pass
+        #else: raise
 
 def replaceSymbols(s):
     s = s.replace("/","").replace("*","").replace("(","").replace(")","")
--- python/fitConfig.py
+++ python/fitConfig.py
@@ -19,10 +19,12 @@ from configManager import configMgr
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+    except:
+        pass
+    #except OSError as exc: # Python >2.5
+        #if exc.errno == errno.EEXIST and os.path.isdir(path):
+            #pass
+        #else: raise
 
 class fitConfig(object):
     """
