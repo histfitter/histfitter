508286f43dfa2290b06e7d3882988f2bebeef2b5
Max Baak <maxbaak@gmail.com>
2014-07-25 14:46:53 +0000

    further cleanup

--- src/Significance.h
+++ src/Significance.h
@@ -42,17 +42,9 @@ namespace StatTools
     Double_t FindS95( Double_t nObs, Double_t nEvtExp, Double_t sumErr, Double_t signald=0, Double_t signalu=50, Double_t dll95=1.35277172704770687, Double_t tol=0.001 );
     Double_t FindXS95( Double_t nObs, Double_t nBkgExp, Double_t nBkgErr, Double_t signalEff, Double_t lumi, Double_t pValue=0.05 );
 
-    Double_t FindXS95( Double_t nObs1, Double_t nBkgExp1, Double_t nBkgErr1, Double_t signalEff1,
-            Double_t nObs2, Double_t nBkgExp2, Double_t nBkgErr2, Double_t signalEff2,
-            Double_t lumi, Double_t dll95=1.35277172704770687, Double_t xsectiond=0, Double_t xsectionu=100000, Double_t tol=0.01 );
-
     Double_t FindSNSigma( Double_t nEvtExp, Double_t sumErr, Double_t nsigma3=3.0, Double_t signald=0, Double_t signalu=100, Double_t tol=0.001 ) ;
     Double_t FindXSNSigma( Double_t nBkgExp, Double_t nBkgErr, Double_t signalEff, Double_t lumi, Double_t nsigma3=3.0, 
             Double_t signalu=100 ) ;
-
-    Double_t FindXSNSigma( Double_t nBkgExp1, Double_t nBkgErr1, Double_t signalEff1, Double_t nBkgExp2, Double_t nBkgErr2, Double_t signalEff2, 
-            Double_t lumi, Double_t nsigma3=3.0, Double_t xsectiond=0, Double_t xsectionu=10000, Double_t tol=0.001 ) ;
-
 }
 
 #endif
