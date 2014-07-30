90a957a0b7ecf84643169386ae60c420c00a6c54
Alex Koutsman <alex.koutsman@gmail.com>
2014-07-30 16:59:18 +0000

    removing channelxml

--- python/channelxml.py
+++
@@ -1,9 +0,0 @@
-from channel import Channel
-from logger import Logger
-
-log = Logger("ChannelXML")
-
-def ChannelXML(*args, **kwargs):
-    log.warn("ChannelXML has been deprecated in favour of Channel")
-    return Channel(*args, **kwargs)
-
