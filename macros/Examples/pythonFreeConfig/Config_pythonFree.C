f38ac5eb3ceac1ab78d80aa1e91355b7cc6f23df
Jeanette Lorenz <jeanette.lorenz@gmx.de>
2014-09-29 13:19:10 +0000

    continue with commenting

--- macros/Examples/pythonFreeConfig/Config_pythonFree.C
+++ macros/Examples/pythonFreeConfig/Config_pythonFree.C
@@ -1,9 +1,38 @@
+/**********************************************************************************
+ * Project: HistFitter - A ROOT-based package for statistical data analysis       *
+ * Package: HistFitter                                                            *
+ * Macro  : Config_pythonFree.C                                                   *
+ * Created: 14 November 2012                                                      *
+ *                                                                                *
+ * Description:                                                                   *
+ *      Fits a workspace without requiring a user defined python configuration    *
+ *      file. Various plots as before and after fit plots,                        *
+ *      likelihood curves and correlation matrices can be produced.               *                            
+ *                                                                                *
+ * Authors:                                                                       *
+ *      HistFitter group, CERN, Geneva                                            *
+ *                                                                                *
+ * Redistribution and use in source and binary forms, with or without             *
+ * modification, are permitted according to the terms listed in the file          *
+ * LICENSE.                                                                       *
+ **********************************************************************************/
+
+
 using namespace std;
 using namespace RooFit;
 using namespace RooStats;
 
+/**
+This macro gives an example on how a workspace can be fitted within HistFitter without relying on a user python configuration file.
+Certain information need to be set for this, including the luminosity, the channels constraining the background and the signal regions, if used.
+Various style option can also be set.
+The macro can plot before and after fit plots, correlation matrices and likelihood curves.
+Note that the user will need to modify the name of the file containing the workspace, as well as the names of control and signal regions and other channels,
+and the value for the luminosity.
+*/
 void Config_pythonFree(){
 
+  //Loading HistFitter library
   gSystem->Load("libSusyFitter.so");
 
   //create configMgr instance
@@ -17,24 +46,26 @@ void Config_pythonFree(){
   // create FitConfig
   FitConfig* fc = mgr->addFitConfig("testFitConfig");
 
-  TString wsFileName = "~koutsman/public/SUSY/Sig_SU_400_500_0_10_P_combined_NormalMeasurement_model.root";  //on lxplus
+  //Name of workspace to be opened
+  TString wsFileName = "Sig_SU_400_500_0_10_P_combined_NormalMeasurement_model.root";
 
   // set FitConfig parameters
   fc->m_inputWorkspaceFileName = wsFileName;
-  fc->m_lumi = 5.83521;
+  fc->m_lumi = 5.83521; //modify luminosity to correct value
   
-  // set background constrain regions
+  // set background constrain regions - change names to the ones in your workspace
   fc->m_bkgConstrainChannels.push_back("WREl_meffInc");
   fc->m_bkgConstrainChannels.push_back("TREl_meffInc");
   fc->m_bkgConstrainChannels.push_back("WRMu_meffInc");
   fc->m_bkgConstrainChannels.push_back("TRMu_meffInc");
-  // SRs taken into account in the fit
+  // SRs taken into account in the fit - modify the names to the names of the signal regions in your workspace
   fc->m_signalChannels.push_back("SR4jTEl_meffInc");
   fc->m_signalChannels.push_back("SR4jTMu_meffInc");
-  //  // SRs NOT taken into account in the fit
+  //  // VRs could be added
   //   fc->m_validationChannels.push_back("SR4jTEl_meffInc");
   //   fc->m_validationChannels.push_back("SR4jTMu_meffInc");
 
+  //specify which plots should be created
   // fit, create _afterFit.root workspace and make plots
   Bool_t drawBeforeFit = kFALSE;
   Bool_t drawAfterFit = kTRUE; 
@@ -43,6 +74,7 @@ void Config_pythonFree(){
   Bool_t plotNLL = kFALSE;
   Util::GenerateFitAndPlot(fc->m_name,  drawBeforeFit,  drawAfterFit,  plotCorrelationMatrix,  plotSeparateComponents,  plotNLL );
   
+  // starting with style settings
   // new ChannelStyle
   ChannelStyle* style = new ChannelStyle("WREl_meffInc");
   
@@ -57,7 +89,7 @@ void Config_pythonFree(){
   style->addSample( "Data",kBlack);
   style->addSample( "SU_400_500_0_10_P",kMagenta);
 
-  // add new channelStyle to fitConfig
+  // add new channelStyle to fitConfig - finished style settings
   fc->m_channelsStyle.push_back(*style);
   
   // draw again, but now with new style for channel WREl_meffInc and also plot correlation matrix of fit
