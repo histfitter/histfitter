8e1a91c5ab9f7377ef1484308d5e3f90b1d2bef6
Jeanette Lorenz <jeanette.lorenz@gmx.de>
2015-05-22 14:38:49 +0000

    update to Root 5.34.30

--- setup_afs.sh
+++ setup_afs.sh
@@ -1,11 +1,11 @@
 # setup ROOT
 # check Root environment setup. Allow for external setup script.
 
-export BUILD="x86_64-slc6-gcc47-opt"
-export ROOTVERSION="5.34.23"
+export BUILD="x86_64-slc6-gcc48-opt"
+export ROOTVERSION="5.34.30"
 export CERNPREFIX="/afs/cern.ch/"
 export PYTHONVERSION="2.7.3"
-export GCCVERSION="4.7"
+export GCCVERSION="4.8"
 
 # Must have gcc and python setup outside of ROOTSYS def for batch running!
 # This section here is cern specific.
@@ -28,15 +28,15 @@ if [ ! $ROOTSYS ]; then
   echo "With build ${BUILD} ..."
   export CWD=$PWD
   # setup corresponding root
-  cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root_v$ROOTVERSION/
-  #cd $CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/
+  #cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root_v$ROOTVERSION/
+  cd $CERNPREFIX/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/
   ###cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root-v5-34/
   ###cd $CERNPREFIX/work/g/gbesjes/root-v5-34-patches
   #cd $CERNPREFIX/atlas/offline/external/FullChainTest/tier0/test/mbaak/root/root-v5-34-trunk/
   source bin/thisroot.sh
   cd $CWD
   # xrootd setup
-  source /afs/cern.ch/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/bin/setxrd.sh /afs/cern.ch/sw/lcg/external/xrootd/3.2.7/$BUILD
+  #source /afs/cern.ch/sw/lcg/app/releases/ROOT/$ROOTVERSION/$BUILD/root/bin/setxrd.sh /afs/cern.ch/sw/lcg/external/xrootd/3.2.7/$BUILD
   ## setup xrootd on top of this
   #export PATH=$CERNPREFIX/sw/lcg/external/xrootd/3.2.7/$BUILD/bin:$PATH
   #export LD_LIBRARY_PATH="$CERNPREFIX/sw/lcg/external/xrootd/3.2.7/$BUILD/lib64:$LD_LIBRARY_PATH"
