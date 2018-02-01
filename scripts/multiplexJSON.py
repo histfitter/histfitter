2d5cf711b8da6022351ad69af626cad61d9504ae
Lawrence Lee Jr <lawrenceleejr@gmail.com>
2018-02-01 20:54:26 +0100

    multiplexJSON: now working with theory uncertainties

--- scripts/multiplexJSON.py
+++ scripts/multiplexJSON.py
@@ -11,7 +11,7 @@
 import json
 import argparse
 import glob
-import os
+import os,sys
 import pandas as pd
 
 parser = argparse.ArgumentParser()
@@ -25,6 +25,7 @@ args = parser.parse_args()
 
 def main():
 
+	print ">>>"
 	print ">>> Welcome to multiplexJSON!"
 
 	# Print out the settings
@@ -47,14 +48,14 @@ def main():
 			databaseList.append(df)
 
 		print ">>> Looking for theory variation files"
-		if os.path.isfile(filename.replace("Nominal","Up")) and os.path.isfile(filename.replace("Nominal","Down")):
+		if "Nominal" in filename and os.path.isfile(filename.replace("Nominal","Up")) and os.path.isfile(filename.replace("Nominal","Down")):
 			with open(filename.replace("Nominal","Up")) as data_file:
-				print ">>> >>> Adding input file: %s"%filename
+				print ">>> >>> Adding input file for theory up variation"
 				data = json.load(data_file)
 				df = pd.DataFrame(data, columns=data[0].keys())
 				databaseListTheoryUp.append(df)
 			with open(filename.replace("Nominal","Down")) as data_file:
-				print ">>> >>> Adding input file: %s"%filename
+				print ">>> >>> Adding input file for theory down variation"
 				data = json.load(data_file)
 				df = pd.DataFrame(data, columns=data[0].keys())
 				databaseListTheoryDown.append(df)
@@ -62,31 +63,48 @@ def main():
 			print ">>> ... Can't find theory variation inputs. Skipping..."
 
 	database = pd.concat(databaseList)
-	databaseTheoryUp   = pd.concat(databaseListTheoryUp)
-	databaseTheoryDown = pd.concat(databaseListTheoryDown)
+	if len(databaseListTheoryUp):
+		databaseTheoryUp   = pd.concat(databaseListTheoryUp)
+		databaseTheoryDown = pd.concat(databaseListTheoryDown)
 
 	# cleaning up the bad stuff!
 	database = database[(database.CLsexp != 0) & database.failedstatus==0]
 
-	listOfModels = df[args.modelDef.split(",")].drop_duplicates()
+	try:
+		listOfModels = df[args.modelDef.split(",")].drop_duplicates()
+	except:
+		print ">>> Problem! The model definition variables don't seem to exist! Quitting."
+		sys.exit(1)
 
 	print ">>> Number of signal models considered: %s"%len(listOfModels)
 
 	outputDB = doTheMuxing(database,listOfModels)
 
-
 	# Handle the theory variation databses using the optimization from the nominal...
 
-
+	if len(databaseListTheoryUp):
+		outputDBTheoryUp   = doTheMuxing(databaseTheoryUp,listOfModels, outputDB)
+		outputDBTheoryDown = doTheMuxing(databaseTheoryDown,listOfModels, outputDB)
 
 	print ">>> Writing output json: %s"%args.outputFile
 
 	with open(args.outputFile, 'w') as f:
 		f.write( outputDB.to_json(orient = "records") )
 
+
+	if len(databaseListTheoryUp):
+		print ">>> Writing output json for theory variations *_[Up/Down].json "
+		with open(args.outputFile.replace(".json","")+"_Up.json", 'w') as f:
+			f.write( outputDBTheoryUp.to_json(orient = "records") )
+		with open(args.outputFile.replace(".json","")+"_Down.json", 'w') as f:
+			f.write( outputDBTheoryDown.to_json(orient = "records") )
+
+	print ">>> Done!"
+	print ">>>"
+
 	return
 
-def doTheMuxing(database,listOfModels):
+def doTheMuxing(database,listOfModels, nominalDatabase=0):
 
 	outputDatabaseList = []
 
@@ -100,11 +118,25 @@ def doTheMuxing(database,listOfModels):
 
 		# Now I've filtered out so I'm only looking at statements for this specific model!
 
-		bestRow = tmpDatabase.loc[[tmpDatabase[args.figureOfMerit].idxmin()]]
+		# Use figure of merit to do optimization
+		if type(nominalDatabase)==int:
+			bestRow = tmpDatabase.loc[[tmpDatabase[args.figureOfMerit].idxmin()]]
+
+		# Use nominalDatabase that has already been optimized for choosing best row
+		else:
+
+			tmpNominalDatabase = nominalDatabase
+			# Select for only those rows that correspond to my model point
+			for item in list(model.index):
+				tmpNominalDatabase = tmpNominalDatabase.loc[tmpNominalDatabase[item] == model[item]]
+
+			# print tmpNominalDatabase.fID
+			bestRow = tmpDatabase.loc[tmpDatabase["fID"]==tmpNominalDatabase.fID]
 
 		outputDatabaseList.append(bestRow)
 
 	return pd.concat(outputDatabaseList)
+
 
 
 if __name__ == "__main__":
