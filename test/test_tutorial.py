918be18737f069579ebb5a5f003460341b5cbb27
Jonathan Long <long.jonathan@gmail.com>
2022-01-17 11:27:33 +0100

    Update test/test_tutorial.py to remove missing file check since PR#107 fixes it.  Also added a note about another check of the stderr disapearing in ROOT 6.24 and added a test that the fit worked in stdout.

--- test/test_tutorial.py
+++ test/test_tutorial.py
@@ -3,7 +3,8 @@ def test_tutorial_MyUserAnalysis(script_runner):
     ret = script_runner(command)
 
     stderr = ret.stderr.read().decode("utf-8")
-    assert "data/MyUserAnalysis.root does not exist" in stderr
+
+    # n.b this Hesse is valid line doesn't  show up in ROOT 6.24, but does with 6.22
     assert (
         "Info in Minuit2Minimizer::Hesse : Hesse is valid - matrix is accurate"
         in stderr
@@ -11,5 +12,6 @@ def test_tutorial_MyUserAnalysis(script_runner):
     assert "results/MyUserAnalysis/fit_parameters.root has been created" in stderr
 
     stdout = ret.stdout.read().decode("utf-8")
+    assert "Status : MINIMIZE=0 HESSE=0" in stdout
     assert "HistFitter: Leaving HistFitter... Bye!" in stdout
     assert "* * * Welcome to HistFitter * * *" in stdout
