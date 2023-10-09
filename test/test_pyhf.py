ed528756bd0dda6418dbb384769b279f020d746a
Aurora <aurora.grefsrud@gmail.com>
2023-10-09 14:32:54 +0200

    Update test

--- test/test_pyhf.py
+++ test/test_pyhf.py
@@ -1,9 +1,6 @@
-def test_tutorial_MyUserAnalysis(script_runner):
+def test_tutorial_MyUserAnalysis_pytest(script_runner):
     command = "HistFitter.py -w --pyhf -j -z -F disc ${HISTFITTER_WORKDIR}/analysis/tutorial/MyUserAnalysis.py"
     (ret,outRaw,errRaw) = script_runner(command)
-
-    stderr = errRaw.decode("utf-8")
-    assert "results/MyUserAnalysis/fit_parameters.root has been created" in stderr
 
     stdout = outRaw.decode("utf-8")
     assert "import pyhf successful" in stdout
