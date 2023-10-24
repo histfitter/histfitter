9a8dc86719e53edf4408c62d8a85915557cc0c53
Aurora <aurora.grefsrud@gmail.com>
2023-10-24 14:54:44 +0200

    Updating test

--- test/test_pyhf.py
+++ test/test_pyhf.py
@@ -1,9 +1,8 @@
-def test_tutorial_MyUserAnalysis_pytest(script_runner):
+def test_tutorial_MyUserAnalysis_pyhf(script_runner):
     command = "HistFitter.py -w --pyhf -j -z -F disc ${HISTFITTER_WORKDIR}/analysis/tutorial/MyUserAnalysis.py"
     (ret,outRaw,errRaw) = script_runner(command)
-
+    assert ret.returncode == 0
     stdout = outRaw.decode("utf-8")
-    stderr = errRaw.decode("utf-8")
     assert "* * * Welcome to HistFitter * * *" in stdout
     assert "import pyhf successful" in stdout
     assert "pyhf command line tool installed" in stdout
